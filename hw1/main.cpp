e8c2ef956a4c43b8a488796ad8ba7eaa4402cb09
Mark Underwood <mtunderwood@alaska.edu>
2019-08-30 16:27:49 -0800

    Fix partition(), add tests

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -6,19 +6,24 @@ using vectorType = std::vector<int>;
 
 vectorType::iterator partition(vectorType& A, vectorType::iterator lo, vectorType::iterator hi)
 {
+    if (hi == A.end())
+    {
+        --hi;
+    }
+
     int pivot = *hi;
     auto i = lo;
 
-    for (auto j = lo; lo <= hi - 1; j++)
+    for (auto j = lo; j <= hi; j++)
     {
         if (*j < pivot)
         {
             std::swap(*i, *j);
             ++i;
         }
-        std::swap(*i, *hi);
     }
 
+    std::swap(*i, *hi);
     return i;
 }
 
@@ -32,39 +37,35 @@ void quicksort(vectorType& A, vectorType::iterator lo, vectorType::iterator hi)
     }
 }
 
-bool isVectorSorted(const vectorType& A)
-{
-    if(A.empty() || A.size() == 1)
-    {
-        return true;
-    }
-
-    for (auto i = A.begin() + 1; i < A.end(); i++)
-    {
-        if (*(i - 1) > *i)
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 TEST_CASE("Basic Functionality")
 {
     std::vector<int> data1{};
     quicksort(data1, data1.begin(), data1.end());
-    REQUIRE(isVectorSorted(data1));
-    REQUIRE(isVectorSorted(vectorType{1,2,3,4}));
-    REQUIRE(!isVectorSorted(vectorType{1,3,2,4}));
+    REQUIRE(data1.empty());
 
     vectorType data2{3};
-    REQUIRE(isVectorSorted(data2));
+    quicksort(data2, data2.begin(), data2.end());
+    REQUIRE(data2 == vectorType{3});
 }
 
 TEST_CASE("Small Sizes")
 {
     vectorType data3{2,124,45,3,34,32,1};
     quicksort(data3, data3.begin(), data3.end());
-    REQUIRE(isVectorSorted(data3));
+    REQUIRE(data3 == vectorType{1,2,3,32,34,45,124});
+
+    vectorType data4{4,4,4,4,4,4,4};
+    quicksort(data4, data4.begin(), data4.end());
+    REQUIRE(data4 == vectorType{4,4,4,4,4,4,4});
+
+    vectorType data5{4,5,4,5,4,5,4};
+    quicksort(data5, data5.begin(), data5.end());
+    REQUIRE(data5 == vectorType{4,4,4,4,5,5,5});
+}
+
+TEST_CASE("Nearly-Sorted Data")
+{
+    vectorType data6{1,2,3,4,6,5,7,8};
+    quicksort(data6, data6.begin(), data6.end());
+    REQUIRE(data6 == vectorType{1,2,3,4,5,6,7,8});
 }
